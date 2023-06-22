1d8e358df3ad824b298d2269bab2d8b150d4b278
sidgabrielmoura <sidgabrielmoura40@gmail.com>
2023-06-21 21:35:14 -0300

    text bug

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -7,13 +7,13 @@ export function NavBar() {
         sm:px-5 sm:pr-10'>
 
             <Link href={"/"} className="hover:text-green-600 transition-all duration-300 text-gray-300
-            sm:text-[13px] md:text-[23px] xl:text-[19px]"><Home /></Link>
+            sm:text-[15px] md:text-[23px] xl:text-[19px]"><Home /></Link>
             <Link href="/contatos" className="hover:text-green-600 transition-all duration-300 text-gray-300
-            sm:text-[13px] md:text-[23px] xl:text-[19px]">contatos</Link>
+            sm:text-[15px] md:text-[23px] xl:text-[19px]">contatos</Link>
             <Link href="/projects" className="hover:text-green-600 transition-all duration-300 text-gray-300
-            sm:text-[13px] md:text-[20px] xl:text-[19px] ">projetos</Link>
+            sm:text-[15px] md:text-[20px] xl:text-[19px] ">projetos</Link>
             <Link href="/about" className="hover:text-green-600 transition-all duration-300 text-gray-300
-            sm:text-[13px] md:text-[23px] xl:text-[19px]">sobre</Link>
+            sm:text-[15px] md:text-[23px] xl:text-[19px]">sobre</Link>
 
         </nav>
     )
